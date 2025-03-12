c95769be96979840f6cdb0ae3d4934f16454892e
On0n0k1 <lucas.lemos.kaihatsusha@gmail.com>
2025-03-12 18:57:54 -0300

    feat: RES-01 Contract as Receiver of ft_transfer
    Signed-off-by: On0n0k1 <lucas.lemos.kaihatsusha@gmail.com>

--- session_vault/src/account.rs
+++ session_vault/src/account.rs
@@ -2,11 +2,9 @@ use near_contract_standards::fungible_token::core_impl::ext_fungible_token;
 use near_contract_standards::fungible_token::receiver::FungibleTokenReceiver;
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 
-use near_sdk::{
-    env, is_promise_success, log, near_bindgen, AccountId, Balance, PromiseOrValue,
-};
 use crate::utils::*;
 use crate::*;
+use near_sdk::{env, is_promise_success, log, near_bindgen, AccountId, Balance, PromiseOrValue};
 
 #[derive(BorshDeserialize, BorshSerialize)]
 pub enum VAccount {
@@ -31,10 +29,10 @@ impl From<Account> for VAccount {
 #[derive(BorshDeserialize, BorshSerialize)]
 pub struct Account {
     pub account_id: AccountId,
-    
+
     // session start time
     pub start_timestamp: TimestampSec,
-    // per session lasts, eg: 90 days 
+    // per session lasts, eg: 90 days
     pub session_interval: TimestampSec,
     // totally how many session, eg: 1
     pub session_num: u32,
@@ -52,15 +50,13 @@ pub struct Account {
 }
 
 impl Account {
-    
     pub(crate) fn unclaimed_amount(&self, cur_ts: u64) -> u128 {
         if self.last_claim_session >= self.session_num {
             return 0_u128;
         }
 
         let cur_session = if cur_ts > to_nano(self.start_timestamp) {
-            ((cur_ts - to_nano(self.start_timestamp))
-                / to_nano(self.session_interval)) as u32
+            ((cur_ts - to_nano(self.start_timestamp)) / to_nano(self.session_interval)) as u32
         } else {
             0
         };
@@ -80,26 +76,29 @@ impl Account {
 }
 
 impl Contract {
-
-    pub fn internal_deposit_to_account(
-        &mut self,
-        account_id: &AccountId,
-        amount: Balance
-    ){
+    pub fn internal_deposit_to_account(&mut self, account_id: &AccountId, amount: Balance) {
         let mut account = self
-                .data()
-                .accounts
-                .get(&account_id)
-                .map(|va| va.into_current())
-                .expect("ERR_ACCOUNT_NOT_EXIST");
-        assert!(account.locking_amount() == 0 && account.last_claim_session != account.session_num, "ERR_ALREADY_DEPOSITED");
-        assert!(account.session_num as Balance * account.release_per_session == amount, "ERR_INCORRECT_AMOUNT");
+            .data()
+            .accounts
+            .get(&account_id)
+            .map(|va| va.into_current())
+            .expect("ERR_ACCOUNT_NOT_EXIST");
+        assert!(
+            account.locking_amount() == 0 && account.last_claim_session != account.session_num,
+            "ERR_ALREADY_DEPOSITED"
+        );
+        assert!(
+            account.session_num as Balance * account.release_per_session == amount,
+            "ERR_INCORRECT_AMOUNT"
+        );
 
         account.deposited_amount += amount;
-        self.data_mut().accounts.insert(&account_id, &account.into());
+        self.data_mut()
+            .accounts
+            .insert(&account_id, &account.into());
         self.data_mut().total_balance += amount;
     }
-    
+
     pub fn internal_add_account(
         &mut self,
         account_id: AccountId,
@@ -110,15 +109,24 @@ impl Contract {
     ) -> bool {
         if let Some(acc) = self.data().accounts.get(&account_id) {
             let mut account = acc.into_current();
-            assert!(to_nano(account.start_timestamp + account.session_num * account.session_interval)
-                < env::block_timestamp(), "ERR_ACCOUNT_IN_SESSION");
-            assert_eq!(0, account.unclaimed_amount(env::block_timestamp()), "ERR_ACCOUNT_NEED_CLAIM");
+            assert!(
+                to_nano(account.start_timestamp + account.session_num * account.session_interval)
+                    < env::block_timestamp(),
+                "ERR_ACCOUNT_IN_SESSION"
+            );
+            assert_eq!(
+                0,
+                account.unclaimed_amount(env::block_timestamp()),
+                "ERR_ACCOUNT_NEED_CLAIM"
+            );
             account.start_timestamp = start_timestamp;
             account.session_interval = session_interval;
             account.session_num = session_num;
             account.release_per_session = release_per_session;
             account.last_claim_session = 0;
-            self.data_mut().accounts.insert(&account_id, &account.into());
+            self.data_mut()
+                .accounts
+                .insert(&account_id, &account.into());
         } else {
             let account = Account {
                 account_id: account_id.clone(),
@@ -130,7 +138,9 @@ impl Contract {
                 claimed_amount: 0,
                 deposited_amount: 0,
             };
-            self.data_mut().accounts.insert(&account_id, &account.into());
+            self.data_mut()
+                .accounts
+                .insert(&account_id, &account.into());
         }
         true
     }
@@ -138,16 +148,17 @@ impl Contract {
 
 #[near_bindgen]
 impl Contract {
-    
     pub fn claim(&mut self, account_id: Option<ValidAccountId>) -> PromiseOrValue<bool> {
-        let account_id = account_id.map(|va| va.into()).unwrap_or(env::predecessor_account_id());
+        let account_id = account_id
+            .map(|va| va.into())
+            .unwrap_or(env::predecessor_account_id());
         let mut account = self
-                .data()
-                .accounts
-                .get(&account_id)
-                .map(|va| va.into_current())
-                .expect("ERR_ACCOUNT_NOT_EXIST");
-             
+            .data()
+            .accounts
+            .get(&account_id)
+            .map(|va| va.into_current())
+            .expect("ERR_ACCOUNT_NOT_EXIST");
+
         if account.last_claim_session > 0 && account.last_claim_session >= account.session_num {
             // all token has been claimed.
             return PromiseOrValue::Value(false);
@@ -157,17 +168,16 @@ impl Contract {
             return PromiseOrValue::Value(true);
         }
 
-        assert!(
-            amount <= account.locking_amount(),
-            "ERR_NOT_ENOUGH_BALANCE"
-        );
+        assert!(amount <= account.locking_amount(), "ERR_NOT_ENOUGH_BALANCE");
 
         let sessions = (amount / account.release_per_session) as u32;
         account.last_claim_session += sessions;
         account.claimed_amount += amount;
 
         self.data_mut().claimed_balance += amount;
-        self.data_mut().accounts.insert(&account_id, &account.into());
+        self.data_mut()
+            .accounts
+            .insert(&account_id, &account.into());
 
         ext_fungible_token::ft_transfer(
             account_id.clone(),
@@ -206,7 +216,9 @@ impl Contract {
             account.claimed_amount -= amount.0;
 
             self.data_mut().claimed_balance -= amount.0;
-            self.data_mut().accounts.insert(&account_id, &account.into());
+            self.data_mut()
+                .accounts
+                .insert(&account_id, &account.into());
             log!(
                 "Account claim failed and rollback, account is {}, balance is {}",
                 account_id,
@@ -223,7 +235,6 @@ impl Contract {
     }
 }
 
-
 #[near_bindgen]
 impl FungibleTokenReceiver for Contract {
     /// Callback on receiving tokens by this contract.
@@ -233,7 +244,6 @@ impl FungibleTokenReceiver for Contract {
         amount: WrappedBalance,
         msg: String,
     ) -> PromiseOrValue<WrappedBalance> {
-
         let token_in = env::predecessor_account_id();
         let amount: Balance = amount.into();
         assert_eq!(token_in, self.data().token_account_id, "ERR_ILLEGAL_TOKEN");
@@ -241,16 +251,15 @@ impl FungibleTokenReceiver for Contract {
         if msg.is_empty() {
             env::panic(b"ERR_MISSING_ACCOUNT_ID");
         } else {
+            let contract_id: String = env::current_account_id();
+            if msg.eq_ignore_ascii_case(contract_id.as_str()) {
+                env::panic(b"ERR_RECIPIENT_CANNOT_BE_SESSION_VAULT_CONTRACT");
+            }
             self.internal_deposit_to_account(&msg, amount);
         }
 
         let sender: AccountId = sender_id.into();
-        log!(
-            "{} deposit token to {}, amount: {}",
-            sender,
-            msg,
-            amount
-        );
+        log!("{} deposit token to {}, amount: {}", sender, msg, amount);
         PromiseOrValue::Value(0.into())
     }
 }
